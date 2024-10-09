687da6c77b45918910a62d2b2bb262aee863da93
iSathyam31 <sathyam.a31@gmail.com>
2024-10-09 21:12:24 +0530

    Project Structure Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'D.A.I.S.Y',
+    version= '0.0.0',
+    author= 'Sathyam A',
+    author_email= 'sathyam.a31@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
